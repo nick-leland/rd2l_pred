d5edb9874417a52a4220f98f371bc5b07fa030e5
Nicholas Leland <nicholasrleland@gmail.com>
2025-01-25 23:24:17 -0500

    Updated imports

--- random_forest_model_training.py
+++ random_forest_model_training.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 
 
